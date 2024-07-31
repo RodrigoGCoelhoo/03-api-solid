f1bec2eec15be5f9e8314e4a2624be79c7b43262
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2024-07-30 21:36:21 -0300

    fix: import beforeEach from vitest

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,9 +1,8 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
-import { beforeEach } from 'node:test'
 
 let usersRepository: InMemoryUsersRepository
 let registerUseCase: RegisterUseCase
