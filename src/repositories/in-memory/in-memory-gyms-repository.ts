5d0c43b18c3694aab9bcb7235a662040bd4afd95
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2024-08-05 21:10:58 -0300

    feat: search gym by name use case + tests

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -11,6 +11,12 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym
   }
 
+  async searchMany(query: string, page: number) {
+    const gyms = this.gyms.filter((gym) => gym.title.includes(query))
+
+    return gyms.slice((page - 1) * 20, page * 20)
+  }
+
   async create(data: Prisma.GymCreateInput) {
     const gym = {
       id: data.id ?? randomUUID(),
---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,70 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { SearchGymsUseCase } from './search-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: SearchGymsUseCase
+
+describe('Search Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new SearchGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to serach for gyms', async () => {
+    await gymsRepository.create({
+      title: 'Ironberg',
+      description: null,
+      phone: null,
+      latitude: -23.506472897932845,
+      longitude: -46.878660828764644,
+    })
+
+    await gymsRepository.create({
+      title: 'SmartFit',
+      description: null,
+      phone: null,
+      latitude: -23.506472897932845,
+      longitude: -46.878660828764644,
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'Ironberg',
+      page: 1,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([
+      expect.objectContaining({
+        title: 'Ironberg',
+      }),
+    ])
+  })
+
+  it('should be able to fetch paginated users check-in history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `Ironberg ${i}`,
+        description: null,
+        phone: null,
+        latitude: -23.506472897932845,
+        longitude: -46.878660828764644,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'Ironberg',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({
+        title: 'Ironberg 21',
+      }),
+      expect.objectContaining({
+        title: 'Ironberg 22',
+      }),
+    ])
+  })
+})
